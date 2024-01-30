34b56bd3ea1c8ed0551dba50c9f5cb5ebd46703a
AndruQM <quispemoyaa@gmail.com>
2024-01-30 10:07:44 -0400

    agregar base proyecto editor

---
+++ src/editor/Principal.java
@@ -0,0 +1,26 @@
+package editor;
+
+import javax.swing.*;
+
+public class Principal {
+	public static void main(String []args) {
+		DimencionesEditor marco=new DimencionesEditor();
+		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		marco.setVisible(true);
+	}
+}
+	class DimencionesEditor extends JFrame{
+		public DimencionesEditor() {
+			setBounds(100,100,500,500);
+			setTitle("Editor Personalizado");
+			add(new Panel());
+		}
+		
+	}
+	class Panel extends JPanel{
+		public Panel() {
+			JLabel etiqueta= new JLabel("HOLA MUNDO");
+			add(etiqueta);
+		}
+		
+	}
