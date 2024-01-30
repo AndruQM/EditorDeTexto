e20f0222345a5849e33acb3381d450ed7ad29a8c
AndruQM <quispemoyaa@gmail.com>
2024-01-30 17:56:14 -0400

    avance menu con items

--- src/editor/Principal.java
+++ src/editor/Principal.java
@@ -22,7 +22,49 @@ public class Principal {
 		private JTabbedPane tPane ;
 		private JTabbedPane ventana ;
 		private JTextArea areaDeTexto;
+		private JMenuBar menu;
+		private JMenu archivo, editar, seleccionar, ver, apariencia;
+		private JMenuItem elementItem;
 		public Panel() {
+			//-------------------MENU---------------------------
+			JPanel panelMenu =new JPanel();
+			menu=new JMenuBar();
+			archivo= new JMenu("Archivo");
+			editar= new JMenu("Editar");
+			seleccionar= new JMenu("Seleccion");
+			ver= new JMenu("ver");
+			apariencia= new JMenu("Aparencia");
+
+			menu.add(archivo);
+			menu.add(editar);
+			menu.add(seleccionar);
+			menu.add(ver);
+			
+			//items Archivos
+			crearItem("Nuevo Archivo", "archivo", "nuevo");
+			crearItem("Abrir Archivo", "archivo", "abrir");
+			crearItem("Guardar", "archivo", "guardar");
+			crearItem("Guardar Como", "archivo", "guardar como");
+
+			//items Editar
+			crearItem("Deshacer", "editar", "");
+			crearItem("Rehacer", "editar", "");
+			crearItem("Deshacer", "editar", "");
+			crearItem("Cortar", "editar", "");
+			crearItem("Copiar", "editar", "");
+			crearItem("Pegar", "editar", "");
+
+			//items Seleccionar
+			crearItem("Seleccionar Todo", "seleccionar", "");
+
+			//items Ver
+			crearItem("Numeracion", "ver", "");
+			ver.add(apariencia);
+			crearItem("Modo Blanco", "apariencia", "");
+			crearItem("Modo Oscuro", "apariencia", "");
+			panelMenu.add(menu);
+
+			//--------------------------------------------------
 			/*
 			JLabel etiqueta= new JLabel("editor de texto personalizado echo por mi");
 			add(etiqueta);*/
@@ -30,6 +72,7 @@ public class Principal {
 			tPane=new JTabbedPane();
 			//----------------------------------------------
 			crearPlanel();
+			add(panelMenu);
 			add(tPane);
 			
 
@@ -39,6 +82,23 @@ public class Principal {
 			areaDeTexto = new JTextArea();
 			ventana.add(areaDeTexto);
 			tPane.addTab("title", ventana);
+		}
+		public void crearItem(String rotulo, String menu, String accion){
+			elementItem=new JMenuItem(rotulo);
+			if (menu.equals("archivo")) {
+				archivo.add(elementItem);
+			} else if (menu.equals("editar")) {
+				editar.add(elementItem);
+			}else if (menu.equals("seleccionar")) {
+				seleccionar.add(elementItem);
+			} else if (menu.equals("ver")) {
+				ver.add(elementItem);
+			}else if (menu.equals("apariencia")) {
+				apariencia.add(elementItem);
+			}
+				
+			
+
 		}
 
 		
