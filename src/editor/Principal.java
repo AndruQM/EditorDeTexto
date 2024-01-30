77fadeae88b5398ff33887806264bf76985d8bbe
AndruQM <quispemoyaa@gmail.com>
2024-01-30 16:42:56 -0400

    avance encabezado de titulo

--- src/editor/Principal.java
+++ src/editor/Principal.java
@@ -12,16 +12,35 @@ public class Principal {
 }
 	class DimencionesEditor extends JFrame{
 		public DimencionesEditor() {
-			setBounds(100,100,500,500);
+			setBounds(500,100,500,500);
 			setTitle("Editor Personalizado AndruQM");
 			add(new Panel());
 		}
 		
 	}
 	class Panel extends JPanel{
+		private JTabbedPane tPane ;
+		private JTabbedPane ventana ;
+		private JTextArea areaDeTexto;
 		public Panel() {
+			/*
 			JLabel etiqueta= new JLabel("editor de texto personalizado echo por mi");
-			add(etiqueta);
+			add(etiqueta);*/
+			//---------------area de texto------------------
+			tPane=new JTabbedPane();
+			//----------------------------------------------
+			crearPlanel();
+			add(tPane);
+			
+
 		}
+		public void crearPlanel(){
+			ventana = new JTabbedPane();
+			areaDeTexto = new JTextArea();
+			ventana.add(areaDeTexto);
+			tPane.addTab("title", ventana);
+		}
+
+		
 		
 	}
